ab5cbabb45b84ac57c28ef8ed79d74bc50d5fc45
Ryo Takahashi <ks22283tr@tcue.ac.jp>
2024-08-11 17:06:29 +0900

    [3の倍数]を学習しました。

--- 01_テキスト/01_JavaScriptのオブジェクト/main.js
+++ 01_テキスト/01_JavaScriptのオブジェクト/main.js
@@ -16,11 +16,21 @@ const hikakin = {
 // hikakin オブジェクト を真似して自分のプロフィールをオブジェクトで作ってください。
 // オブジェクトにはたくさんのプロパティを追加してください。
 
-// const me = { name: 'あなたのなまえ' }
-
+const me = { 
+  name: 'Ryo',
+age:20,
+hobbies:["読書","乗り鉄","撮り鉄"],
+isHappy: true,
+address:{
+  country:"日本",
+  city:"高崎",
+},
+}
+console.log(me);
 // ブラウザのコンソールを開いてオブジェクトが正しく作れているか確認してみましょう👀
 
 // console.log(me)
 
 // TRY: 練習問題２
 // hikakin オブジェクトに入っている sayHello メソッドを実行してください。
+hikakin.sayHello()
